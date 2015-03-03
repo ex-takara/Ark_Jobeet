0a5487e4d5b8a323411946b9a3014093d9c93ab4
taka <taka2063jp@gmail.com>
2015-03-03 15:46:12 +0900

    08日目: カテゴリページで遊ぶ

--- config.pl
+++ config.pl
@@ -2,6 +2,7 @@ return {
     default_view    => 'MT',
     active_days     => 30,
 	max_jobs_on_homepage => 10,
+	max_jobs_on_category => 20,
     database => [
         'dbi:mysql:jobeet', 'root', '',
          {
